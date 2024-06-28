61c01a19ab54a7578f9c0d8b55f55c6d98073ecd
zengxw <zengxw@getui.com>
2024-06-28 11:43:17 +0800

    [feature]:pigeon-post-web接入springboot

--- pigeon-post-web/src/main/java/com/seeing/pigeonpostweb/PigeonPostWebApplication.java
+++ pigeon-post-web/src/main/java/com/seeing/pigeonpostweb/PigeonPostWebApplication.java
@@ -1,9 +1,13 @@
 package com.seeing.pigeonpostweb;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class PigeonPostWebApplication {
 
 	public static void main(String[] args) {
+		SpringApplication.run(PigeonPostWebApplication.class, args);
 	}
 
 }
