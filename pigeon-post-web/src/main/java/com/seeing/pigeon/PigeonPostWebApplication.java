e3096af799a15804dd124aeb71b0a6d57f9dd693
zengxw <zengxw@getui.com>
2024-07-09 14:33:21 +0800

    [feature]:分布式配置中心配置接入

--- pigeon-post-web/src/main/java/com/seeing/pigeon/PigeonPostWebApplication.java
+++ pigeon-post-web/src/main/java/com/seeing/pigeon/PigeonPostWebApplication.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PigeonPostWebApplication {
 
 	public static void main(String[] args) {
+		/**
+		 * 如果你需要启动Apollo动态配置
+		 * 1、启动apollo
+		 * 2、将application.properties配置文件的 austin.apollo.enabled 改为true
+		 * 3、下方的property替换真实的ip和port
+		 */
+		System.setProperty("apollo.config-service", "http://119.91.205.248:8080");
 		SpringApplication.run(PigeonPostWebApplication.class, args);
 	}
 
